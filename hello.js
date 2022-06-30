39a28e487e0b2e550b7ad0b7c2550468836176d2
karuna devkota <karunadevkota@github.com>
2022-06-30 09:20:18 +0545

    js

---
+++ hello.js
@@ -0,0 +1,35 @@
+function hello()
+{
+    console.log("welcome to javascript")
+}
+hello();
+function test(a,b)
+{
+    return(a+b)
+}
+console.log("result is",test(2,3));
+
+for(i=0;i<4;i++)
+{
+    console.log(i);
+}
+const array =["apple","grapes","mango"];
+for(let i=0;i<array.length;i++)
+{
+    console.log(array[i]);
+}
+let a={name:'karuna'}
+console.log(a.name);
+
+function test(x,y)
+{
+    
+    if(x===y )
+    {
+        console.log("Equal")
+    }
+    else{
+        console.log("Not Equal")
+    }
+}
+    test(10,20);
